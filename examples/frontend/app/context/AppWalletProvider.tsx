5e3a457b9275de7c3bc642dd92972229047328b9
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-10-26 18:13:32 +0900

    fix vercel error

--- examples/frontend/app/context/AppWalletProvider.tsx
+++ examples/frontend/app/context/AppWalletProvider.tsx
@@ -8,7 +8,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 // import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 // Default styles that can be overridden by your app
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function AppWalletProvider({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Devnet;
--- examples/frontend/app/context/UtilProvider.tsx
+++ examples/frontend/app/context/UtilProvider.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const UtilContext = createContext<any>({});
 
 /**
